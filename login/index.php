e4d8dfb88a245f7b5272789fba7cce22bb71126b
Chris <chriscarrillo@outlook.com>
2018-03-01 09:28:50 -0800

    Changed the file structure

--- login/index.php
+++ login/index.php
@@ -2,11 +2,11 @@
 <html>
 <head>
     <title>Reciplease.com | Login</title>
-    <link rel="icon" type="images/png" href="images/favicon.png" />
-    <link rel="shortcut icon" href="images/favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="styles/default.css" />
-    <link rel="stylesheet" type="text/css" href="styles/medium.css" media="screen and (max-width: 1100px)" />
-    <link rel="stylesheet" type="text/css" href="styles/small.css" media="screen and (max-width: 750px)" />
+    <link rel="icon" type="images/png" href="../images/favicon.png" />
+    <link rel="shortcut icon" href="../images/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="../styles/default.css" />
+    <link rel="stylesheet" type="text/css" href="../styles/medium.css" media="screen and (max-width: 1100px)" />
+    <link rel="stylesheet" type="text/css" href="../styles/small.css" media="screen and (max-width: 750px)" />
 </head>
 <body>
 <div id="login">
@@ -16,7 +16,7 @@
     <div id="loginBox" class="centered">
         <div id="loginLogo">
             <?php
-            include "logo.php";
+            include "../logo.php";
             ?>
         </div>
         <h1>log in</h1>
