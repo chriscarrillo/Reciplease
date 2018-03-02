14d928866de9ae7c88dddcca966b86f2cd15ed71
Chris <chriscarrillo@outlook.com>
2018-03-02 10:41:20 -0800

    Added Kotlin JS and started registration page

--- login/index.php
+++ login/index.php
@@ -7,27 +7,30 @@
     <link rel="stylesheet" type="text/css" href="../styles/default.css" />
     <link rel="stylesheet" type="text/css" href="../styles/medium.css" media="screen and (max-width: 1100px)" />
     <link rel="stylesheet" type="text/css" href="../styles/small.css" media="screen and (max-width: 750px)" />
+    <script type="text/javascript" src="../js/kotlin.js"></script>
+    <script type="text/javascript" src="../js/kotlinx-html-js.js"></script>
+    <script type="text/javascript" src="../js/main.js"></script>
 </head>
 <body>
 <div id="login">
-    <div id="redLoginBox">
-        <div id="loginCircle"></div>
+    <div id="redBox">
+        <div id="circle"></div>
     </div>
-    <div id="loginBox" class="centered">
-        <div id="loginLogo">
+    <div id="centerBox" class="centered">
+        <div id="logo">
             <?php
             include "../logo.php";
             ?>
         </div>
         <h1>log in</h1>
-        <form action="" method="post" id="loginForm">
+        <form action="" method="post" id="loginForm" class="form">
             <input type="text" id="username" name="username" placeholder="username" /><br />
             <input type="password" id="password" name="password" placeholder="password" /><br />
             <div id="links">
-                <a href="register.php" title="register">register</a>
-                <a href="forgot.php" title="forgot username / password">forgot username / password</a>
+                <a href="../register" title="register">register</a>
+                <a href="../forgot" title="forgot username / password">forgot username / password</a>
             </div>
-            <input type="submit" id="loginBtn" name="loginBtn" value="submit" />
+            <input type="submit" id="loginBtn" class="button" name="loginBtn" value="submit" />
         </form>
         
     </div>
--- register/index.php
+++ register/index.php
@@ -7,7 +7,32 @@
     <link rel="stylesheet" type="text/css" href="../styles/default.css" />
     <link rel="stylesheet" type="text/css" href="../styles/medium.css" media="screen and (max-width: 1100px)" />
     <link rel="stylesheet" type="text/css" href="../styles/small.css" media="screen and (max-width: 750px)" />
+    <script type="text/javascript" src="../js/kotlin.js"></script>
+    <script type="text/javascript" src="../js/kotlinx-html-js.js"></script>
+    <script type="text/javascript" src="../js/main.js"></script>
 </head>
 <body>
+<div id="register">
+    <div id="redBox">
+        <div id="circle"></div>
+    </div>
+    <div id="centerBox" class="centered">
+        <div id="logo">
+            <?php
+            include "../logo.php";
+            ?>
+        </div>
+        <h1>register</h1>
+        <form action="" method="post" id="registerForm" class="form">
+            <input type="text" id="firstName" name="firstName" placeholder="first name" /><br />
+            <input type="text" id="lastName" name="lastName" placeholder="last name" /><br />
+            <input type="email" id="email" name="email" placeholder="email" /><br />
+            <input type="text" id="username" name="username" placeholder="username" /><br />
+            <input type="password" id="password" name="password" placeholder="password" /><br />
+            <input type="date" id="dob" name="dob" placeholder="date of birth" /><br />
+            <input type="submit" id="registerBtn" class="button" name="registerBtn" value="register" />
+        </form>
+    </div>
+</div>
 </body>
 </html>
