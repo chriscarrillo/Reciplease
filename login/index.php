5801b128251ed6b961c698041bfd83e5f9945ae8
Curtis <curtisphohl@gmail.com>
2018-03-03 14:53:17 -0800

    Fixed max-width

--- login/index.php
+++ login/index.php
@@ -6,7 +6,7 @@
     <link rel="shortcut icon" href="../images/favicon.ico" />
     <link rel="stylesheet" type="text/css" href="../styles/default.css" />
     <link rel="stylesheet" type="text/css" href="../styles/medium.css" media="screen and (max-width: 1100px)" />
-    <link rel="stylesheet" type="text/css" href="../styles/small.css" media="screen and (max-width: 300px)" />
+    <link rel="stylesheet" type="text/css" href="../styles/small.css" media="screen and (max-width: 750px)" />
     <script type="text/javascript" src="../js/kotlin.js"></script>
     <script type="text/javascript" src="../js/kotlinx-html-js.js"></script>
     <script type="text/javascript" src="../js/main.js"></script>
