7a42bed45ac7c8e91a5be610cc3bc64f4006376f
Chris <chriscarrillo@outlook.com>
2018-03-05 11:21:19 -0800

    Changed .com to .org

--- templates/logo.php
+++ templates/logo.php
@@ -1,9 +1,9 @@
 <div class="logo">
-    <a href="index.php" title="reciplease.com">
+    <a href="../index.php" title="reciplease.org">
         <div id="innerCircle"></div>
         <div id="outerCircle"></div>
         <p class="centered">
-            reciplease<br /><span id="dotCom">.com</span>
+            reciplease<br /><span id="dotOrg">.org</span>
         </p>
     </a>
 </div>
