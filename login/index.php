16efbdb9e5173dbc6dfdead440e3322952b4b1a7
Chris <chriscarrillo@outlook.com>
2018-03-05 11:07:04 -0800

    Created head.php to reducing redundancy

--- login/index.php
+++ login/index.php
@@ -2,14 +2,7 @@
 <html>
 <head>
     <title>Reciplease.com | Login</title>
-    <link rel="icon" type="images/png" href="../images/favicon.png" />
-    <link rel="shortcut icon" href="../images/favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="../styles/default.css" />
-    <link rel="stylesheet" type="text/css" href="../styles/medium.css" media="screen and (max-device-width: 1100px)" />
-    <link rel="stylesheet" type="text/css" href="../styles/small.css" media="screen and (max-device-width: 750px)" />
-    <script type="text/javascript" src="../js/kotlin.js"></script>
-    <script type="text/javascript" src="../js/kotlinx-html-js.js"></script>
-    <script type="text/javascript" src="../js/main.js"></script>
+    <?php include "head.php" ?>
 </head>
 <body>
 <div id="login">
