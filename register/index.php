ad2a12cf2ad2f6a94a6a6ee61ee9265320be42fc
Chris <chriscarrillo@outlook.com>
2018-03-06 11:18:24 -0800

    Rearranged the registration form

--- register/index.php
+++ register/index.php
@@ -17,13 +17,23 @@
         </div>
         <h1>register</h1>
         <form action="" method="post" id="registerForm" class="form">
-            <input type="text" id="firstName" name="firstName" placeholder="first name" /><br />
-            <input type="text" id="lastName" name="lastName" placeholder="last name" /><br />
-            <input type="email" id="email" name="email" placeholder="email" /><br />
-            <input type="text" id="username" name="username" placeholder="username" /><br />
-            <input type="password" id="password" name="password" placeholder="password" /><br />
-            <input type="date" id="dob" name="dob" placeholder="date of birth" /><br />
-            <input type="submit" id="registerBtn" class="button" name="registerBtn" value="register" />
+            <div class="half">
+                <input type="text" id="firstName" class="mobile" name="firstName" placeholder="first name" /><br />
+                <input type="text" id="lastName" class="mobile" name="lastName" placeholder="last name" /><br />
+                <input type="email" id="email" class="mobile" name="email" placeholder="email" /><br />
+                <select>
+                    <option value="0">Favorite Type of Food:</option>
+                    <option value="Italian">Italian</option>
+                    <option value="Mexican">Mexican</option>
+                    <option value="">Other</option>
+                </select>
+            </div>
+            <div class="half">
+                <input type="text" id="username" class="mobile" name="username" placeholder="username" /><br />
+                <input type="password" id="password" class="mobile" name="password" placeholder="password" /><br />
+                <input type="date" id="dob" name="dob" class="mobile" placeholder="date of birth" /><br />
+            </div>
+            <input type="submit" id="registerBtn" class="button mobile" name="registerBtn" value="register" />
         </form>
     </div>
 </div>
