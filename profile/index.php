3a68a17810a27cc35df6a726e1cb09994f1628d6
Chris <chriscarrillo@outlook.com>
2018-03-21 14:07:04 -0700

    Added input fields for profile page

--- profile/index.php
+++ profile/index.php
@@ -9,6 +9,17 @@
 <?php
     include "../templates/header.php";  
 ?>
+    <div id="centerBox" class="centered">
+        <h1>curtis's profile</h1>
+        <form action="" method="post" id="profileForm" class="form">
+            <input type="text" id="firstName" name="firstName" placeholder="first name" /><br /> <!-- The placeholder should be the name in the db -->
+            <input type="text" id="lastName" name="lastName" placeholder="last name" /><br /> <!-- The placeholder should be the name in the db -->
+            <input type="text" id="username" name="username" placeholder="username" /><br />
+            <input type="password" id="password" name="password" placeholder="password" /><br />
+            <input type="email" id="email" name="email" placeholder="email" /><br />
+            <input type="file" id="profilePicture" name="profilePicture" /><br />
+        </form>
+    </div>
 </div>
 </body>
 </html>
