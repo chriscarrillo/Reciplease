b409e61d12563d08d468e50767e386ba0e8f8b85
chrisfernandes102 <christopherjoseph.fernandes@calbaptist.edu>
2018-03-20 20:54:28 -0700

    Finished the title and tabs for the Home Page

--- index.php
+++ index.php
@@ -24,6 +24,17 @@
     <form action="" method="get" id="searchForm" class="form">
             <input type="textSearch" id="search" name="search" placeholder="search" /><br />
     </form>
+    <div id="title">
+        <h1 id="title1">your recipes...</h1>
+        <h1 id="title2">prepared fresh daily</h1>
+    </div>
+    <div id="tabs">
+        <ul>
+            <li><h1 href="/Reciplease/index.php">home</h1></li>
+            <li><a href="#" class="w3-bar-item w3-button">popular</a></li>
+            <li><a href="#" class="w3-bar-item w3-button">pantry</a></li>
+        </ul>
+    </div>
 <?php
     include "templates/profilepic.php";
     include "templates/recipecard.php";
