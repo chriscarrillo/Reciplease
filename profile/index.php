9219a31e705a22eb413c0db08f7d43243596be9c
Chris <chriscarrillo@outlook.com>
2018-03-21 11:47:20 -0700

    Added the profile page

---
+++ profile/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reciplease.com | Profile</title>
+    <?php include "../templates/head.php" ?>
+</head>
+<body>
+<div id="profile">
+</div>
+</body>
+</html>
