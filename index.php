81a77d1b41c70428bd710b835fbde42438463de2
chrisfernandes102 <christopherjoseph.fernandes@calbaptist.edu>
2018-03-21 11:52:28 -0700

    Added the header to a template

--- index.php
+++ index.php
@@ -13,29 +13,10 @@
     <script type="text/javascript" src="js/main.js"></script>
     <script src="js/infinite-scroll.pkgd.js"></script>
     <!-- see here for infinite scroll implementation - https://infinite-scroll.com/-->
-    
 </head>  
 <body id="homePage">
-    <div id="home">
-        <?php
-        include "templates/logo.php";
-        ?>
-    </div>
-    <form action="" method="get" id="searchForm" class="form">
-            <input type="textSearch" id="search" name="search" placeholder="search" /><br />
-    </form>
-    <div id="title">
-        <h1 id="title1">your recipes...</h1>
-        <h1 id="title2">prepared fresh daily</h1>
-    </div>
-    <div id="tabs">
-        <ul>
-            <li><h1 href="/Reciplease/index.php">home</h1></li>
-            <li><a href="#" class="w3-bar-item w3-button">popular</a></li>
-            <li><a href="#" class="w3-bar-item w3-button">pantry</a></li>
-        </ul>
-    </div>
 <?php
+    include "templates/header.php";
     include "templates/profilepic.php";
     include "templates/recipecard.php";
 ?>
