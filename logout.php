ccab2560eedf0a362fe2fc18d53c6e606aa1b38f
Chris <chriscarrillo@outlook.com>
2018-04-11 20:24:30 -0700

    Fixed the logout file

--- logout.php
+++ logout.php
@@ -1,10 +1,12 @@
 <?php
-    # Delete any saved $_SESSION variables
+    session_start();
+
+    // Delete any saved $_SESSION variables
     session_unset();
     
-    # Destroy the active session
+    // Destroy the active session
     session_destroy();
 
-    # Redirect the user to the home page
+    // Redirect the user to the home page
     header("Location: index.php");
 ?>
