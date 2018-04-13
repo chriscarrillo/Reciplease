dde92a784b88efc38a3980319f11afe3edf3f9b5
Chris <chriscarrillo@outlook.com>
2018-04-13 10:49:52 -0700

    Deleted upload.php and updated the path for unirest

--- templates/popularSearch.php
+++ templates/popularSearch.php
@@ -6,10 +6,11 @@
 
 <!-- CALL TO POPULATE POPULAR PAGE -->
         <?php
-        include '../unirest/src/Unirest.php';
+        include "unirest/src/Unirest.php";
     
         // These code snippets use an open-source library. http://unirest.io/php
-        $response = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?l           limitLicense=false&number=2",
+        $response = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?l
+        limitLicense=false&number=2",
         array(
          "X-Mashape-Key" => "dpET0hwYnZmsh4tN4yi4Tx0EW4php1svA7QjsniM24UU0xoOYR",
                 "Accept" => "application/json"
