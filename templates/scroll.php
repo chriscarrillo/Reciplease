e7fd463f68df7d75a64d93439608846f5f4ff43e
curti_000 <curtisphohl@gmail.com>
2018-04-20 10:52:56 -0700

    added check for both values not null

--- templates/scroll.php
+++ templates/scroll.php
@@ -54,14 +54,23 @@
                 "X-Mashape-Key" => "dpET0hwYnZmsh4tN4yi4Tx0EW4php1svA7QjsniM24UU0xoOYR",
                 "Accept" => "application/json"
                 ));
-    } else {
+    } else if (!empty($ingredientsArray) && empty($restrictionsArray)) {
         print "Ingredients array is NOT empty and restrictions array is empty";
         $response = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=".$addRecipeInformation."&fillIngredients=".$fillIngredients."&includeIngredients=".$includeIngredients."&instructionsRequired=".$instructionsRequired."&limitLicense=false&number=".$numberOfResults."&offset=".$offset."&ranking=2",
             array(
                 "X-Mashape-Key" => "dpET0hwYnZmsh4tN4yi4Tx0EW4php1svA7QjsniM24UU0xoOYR",
                 "Accept" => "application/json"
                 ));
+    } else if (!empty($ingredientsArray) && !empty($restrictionsArray)) {
+        print "Ingredients array is NOT empty and restrictions array NOT empty";
+        $response = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?addRecipeInformation=".$addRecipeInformation."&fillIngredients=".$fillIngredients."&includeIngredients=".$includeIngredients."&instructionsRequired=".$instructionsRequired."&limitLicense=false&number=".$numberOfResults."&offset=".$offset."&ranking=2",
+            array(
+                "X-Mashape-Key" => "dpET0hwYnZmsh4tN4yi4Tx0EW4php1svA7QjsniM24UU0xoOYR",
+                "Accept" => "application/json"
+                ));
     }
+
+
 
     //            if ($response != NULL){
     //                echo "Successful API call.";
