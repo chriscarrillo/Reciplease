9af2422bfa06655df44c40833b73af362208c2f2
Chris <chriscarrillo@outlook.com>
2018-04-20 02:12:32 -0700

    Added search functions in main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ $(document).ready(function() {
         $("#redBox").css("opacity", "1");
     }
 });
+
+function search(searchText) {
+    $("#ani_img").show();
+    $.post("../templates/search.php", {search: searchText}, function(data) {
+        if (data.trim().length == 0) {
+            $("#loading").text("End of Page");
+        }
+        $(".results").append(data);
+        $(".well").animate({scrollTop: $("#loading").offset().top}, 5000, "easeOutBounce");
+        $("#ani_img").hide();
+    })
+}
