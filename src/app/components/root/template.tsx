13b0d4a8093e4481e5007bcbf31eca2e0d66acb6
Chris Carrillo <chriscarrillo@outlook.com>
2020-04-03 08:43:45 -0500

    Migrate to Bootstrap (#270)

--- src/app/components/root/template.tsx
+++ src/app/components/root/template.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 /**
  * Render root component representing the entire application
  * @return React component
  */
 export const AppRoot = () => {
-    return <h1>Hey!</h1>
+    return (
+      <>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossOrigin="anonymous"
+        />
+      </>
+    )
 }
