2ba6dc239e47770686fa06f960beb2892c15dd6c
Chris Carrillo <chriscarrillo@outlook.com>
2020-10-25 20:39:40 -0500

    Add base-level Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import 'theme/app.scss'
 
 /**
  * Render base app.
  * @return Base app
  */
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img alt="logo" className="App-logo" src={logo} />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img alt="logo" className="App-logo" src={logo} />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer" target="_blank">
+        Learn React
+      </a>
+    </header>
+  </div>
+)
 
 /**
  * App.
